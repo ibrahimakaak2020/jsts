7bcdabad38680d3084793e461ed9d191ea8a9ebb
ibrahimakaak2020 <ibrahimakaak@gmail.com>
2024-01-16 22:00:01 +0400

    hhh

--- my-app1/src/App.js
+++ my-app1/src/App.js
@@ -1,54 +1,66 @@
-import React, { useEffect, useReducer } from 'react';
-
+import React, { useEffect, useReducer } from 'react'
+/* const url = 'http://localhost:2000/questions';
+const initState = {
+    questions: [],
+    statestatus: "ready",
+    index: 0
+}; */
 const url = 'http://localhost:2000/questions';
 const initState = {
-  questions: [],
-  index: 0,
-  status: '',
-  highScore: 0,
-  score: 0
+    questions: [],
+    index: 0,
+    status: '',
+    highScore: 0,
+    score: 0
 };
+/* function reducer(state, action) {
+    console.log(state);
+    switch (action.type) {
+        case "GET-SUCCESS":
+            return { ...state, statestatus: 'ready', questions: action.payload }
+        case "GET-ERROR":
+            return { ...state, statestatus: 'error' }
 
+        default:
+            throw new Error("Unknown Resources ...");
+    }
+} */
 function reducer(state, action) {
-  switch (action.type) {
-    case 'GET-SUCCESS':
-      return { ...state, status: 'ready', questions: action.payload };
-    case 'GET-ERROR':
-      return { ...state, status: 'error' };
-    default:
-      throw new Error('Action unknown');
-  }
+    switch (action.type) {
+        case 'GET-SUCCESS':
+            return { ...state, status: 'ready', questions: action.payload };
+        case 'GET-ERROR':
+            return { ...state, status: 'error' };
+        default:
+            throw new Error('Action unknown');
+    }
 }
 
 function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initState);
+    // const [{ questions, statestatus, index }, dispatch] = useReducer(reducer, initState);
+    const [{ questions, status, index }, dispatch] = useReducer(reducer, initState);
 
-  useEffect(() => {
-    fetch(url)
-      .then((resp) => resp.json())
-      .then((data) => dispatch({ type: 'GET-SUCCESS', payload: data }))
-      .catch(() => dispatch({ type: 'GET-ERROR' }));
-  }, []);
-console.log(questions);
-  return (
-    <div className='container max-w-7xl mx-auto pt-6'>
-      {status === 'ready' ? (
+    /*    useEffect(    
+           () => {
+               fetch(url)
+               .then((response) => response.json())
+               .then((data) =>{ return dispatch({ type: "SUCCESSFULL", payload: data })})
+               .catch(() => dispatch({ type: "UNSUCCESSFUL" }))}
+          , []); */
+    useEffect(() => {
+        fetch(url)
+            .then((resp) => resp.json())
+            .then((data) => dispatch({ type: 'GET-SUCCESS', payload: data }))
+            .catch(() => dispatch({ type: 'GET-ERROR' }));
+    }, []);
+
+    console.log(questions[index].question)
+
+    return (
         <div>
-          {questions.map((question, index) => (
-            <div key={index} className='card'>
-              <h2>{question.question}</h2>
-              <p>{question.id}</p>
-              {/* Add more JSX to render other properties of the question */}
-            </div>
-          ))}
+            {status === 'ready' ? (<h1>SUCCESS</h1>) : (<div><h1>error in fetching results</h1></div>)}
         </div>
-      ) : status === 'error' ? (
-        <div>Error loading questions</div>
-      ) : (
-        <div>Loading...</div>
-      )}
-    </div>
-  );
-}
+    )
+};
 
 export default App;
---
+++ my-app1/src/App1.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useReducer } from 'react';
+
+const url = 'http://localhost:2000/questions';
+const initState = {
+  questions: [],
+  index: 0,
+  status: '',
+  highScore: 0,
+  score: 0
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'GET-SUCCESS':
+      return { ...state, status: 'ready', questions: action.payload };
+    case 'GET-ERROR':
+      return { ...state, status: 'error' };
+    default:
+      throw new Error('Action unknown');
+  }
+}
+
+function App() {
+  const [{ questions, status ,index}, dispatch] = useReducer(reducer, initState);
+
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => dispatch({ type: 'GET-SUCCESS', payload: data }))
+      .catch(() => dispatch({ type: 'GET-ERROR' }));
+  }, []);
+console.log(questions[index]);
+  return (
+    <div className='container max-w-7xl mx-auto pt-6'>
+      {status === 'ready' ? (
+        <div>
+          {questions.map((question, index) => (
+            <div key={index} className='card'>
+              <h2>{question.question}</h2>
+              <p>{question.id}</p>
+              {/* Add more JSX to render other properties of the question */}
+            </div>
+          ))}
+        </div>
+      ) : status === 'error' ? (
+        <div className='w-96 bg-orange-300'>Error loading questions</div>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
+  );
+}
+
+export default App;
